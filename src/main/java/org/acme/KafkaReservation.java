b59c821c147425e05439d2d70170ba50bc977199
alenlovic <alen.lovic@netsolution.ba>
2023-12-22 16:05:07 +0100

    channel

--- src/main/java/org/acme/KafkaReservation.java
+++ src/main/java/org/acme/KafkaReservation.java
@@ -1,15 +1,22 @@
 package org.acme;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.acme.DTO.ReservationCreateDTO;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+
+import java.util.concurrent.CompletionStage;
 
 @ApplicationScoped
 public class KafkaReservation {
 
-    @Incoming("reservation-topic")
-    public void consumeJsonMessage(ReservationCreateDTO reservationCreateDTO) {
-        System.out.println("Reservation created successfully!  \n" +  reservationCreateDTO);
+    @Incoming("reservation-channel")
+    public CompletionStage<Void> reservationConsumer(Message<ReservationCreateDTO> reservationCreateDTOMessage){
+        ReservationCreateDTO reservationCreateDTO = reservationCreateDTOMessage.getPayload();
+        System.out.println(reservationCreateDTO);
+
+        return reservationCreateDTOMessage.ack();
     }
 }
--- src/main/java/org/acme/ReservationDeserializer.java
+++ src/main/java/org/acme/ReservationDeserializer.java
@@ -3,11 +3,9 @@ package org.acme;
 import io.quarkus.kafka.client.serialization.JsonbDeserializer;
 import org.acme.DTO.ReservationCreateDTO;
 
-
-
 public class ReservationDeserializer extends JsonbDeserializer<ReservationCreateDTO> {
 
-    public ReservationDeserializer(){super(ReservationCreateDTO.class);}
-
-
+    public ReservationDeserializer(){
+        super(ReservationCreateDTO.class);
+    }
 }
